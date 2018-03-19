30bbb1f904909c3bdfec807af7a0dcf73bc63856
Enaell <orelienmartin@hotmail.fr>
2018-03-19 22:10:21 +0100

    [INDEX] add new design base with curtain effect -js css html

---
+++ ExpressApp1/public/javascripts/js/main.js
@@ -0,0 +1,63 @@
+jQuery(document).ready(function($)
+{
+
+	isAnimated = false;
+
+
+	$(document).keydown(function(event)
+	{
+		if( event.which=='38' && !isAnimated) 
+			openSectionBlock($("#welcomeSection .half-block"), $('#mainSection'));
+		 else if( event.which=='40' && !isAnimated) 
+			closeSectionBlock($("#welcomeSection .half-block"), $('#mainSection'))
+	});
+
+	function openSectionBlock(sectionBlockOpened, sectionBlockRevealed)
+	{
+		isAnimated = true;
+
+
+		sectionBlockOpened.addClass('duration2s');
+
+		sectionBlockRevealed.addClass('duration2s');
+
+		sectionBlockOpened.addClass('opened');
+
+		sectionBlockRevealed.addClass('shown');
+
+		setTimeout(function()
+		{
+			sectionBlockOpened.removeClass('duration2s');
+			//sectionBlockOpened.hide();
+
+			sectionBlockRevealed.removeClass('duration2s');
+
+			$("body").css("overflow", "visible");
+
+			isAnimated = false;
+		}, 2000);
+	}
+
+	function closeSectionBlock(sectionBlockClosed, sectionBlockHidden)
+	{
+		isAnimated = true;
+
+		$("body").css("overflow", "hidden");
+
+		sectionBlockClosed.addClass('duration2s');
+
+		sectionBlockHidden.addClass('duration2s');
+
+		sectionBlockClosed.removeClass('opened');
+
+		sectionBlockHidden.removeClass('shown');;
+
+		setTimeout(function(){
+			sectionBlockClosed.removeClass('duration2s');
+
+			sectionBlockHidden.removeClass('duration2s');
+
+			isAnimated = false;
+		}, 2000);
+	}
+});
